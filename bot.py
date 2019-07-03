b9a6074c217dc2e8428dd397e79b34f24298e285
Roman Rakhlin <51785794+romarakhlin@users.noreply.github.com>
2019-07-03 21:44:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def invest(id):
         s = ''
         for i in range(len(config.answers)):
             s += str(config.answers[i]) + '\n'
-        bot.send_message('@loooolff', s)
+        bot.send_message('@dannie_iz_bota', s)
     else:
         bot.send_message(id, my_list[config.n])
  
